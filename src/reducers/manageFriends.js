527a03c6888c1bc99282d87b4bab788c22cebb22
Natalie <natsetc@gmail.com>
2019-10-05 17:53:30 -0700

    spread entire state in return obj

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,10 +1,10 @@
 export function manageFriends(state = {friends: []}, action){
     switch (action.type) {
         case "ADD_FRIEND":
-            return {friends: [...state.friends, action.friend]};
+            return {...state, friends: [...state.friends, action.friend]};
         case "REMOVE_FRIEND":
             const index = state.friends.findIndex(f => f.id === action.id)
-            return {friends: [...state.friends.slice(0,index), ...state.friends.slice(index + 1)]}
+            return {...state, friends: [...state.friends.slice(0,index), ...state.friends.slice(index + 1)]}
         default:
             return state;
     }
